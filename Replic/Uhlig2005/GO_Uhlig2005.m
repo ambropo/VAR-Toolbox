beff20168fd683e0e875be7a3d396d94107abfb3
Ambrogio Cesa-Bianchi <45069084+ambropo@users.noreply.github.com>
2020-11-03 00:59:50 +0000

    upload

--- Replic/Uhlig2005/GO_Uhlig2005.m
+++ Replic/Uhlig2005/GO_Uhlig2005.m
@@ -15,7 +15,7 @@ clear all; clear session; close all; clc
 warning off all
 
 % Load data
-[xlsdata, xlstext] = xlsread('Uhlig2005_Data.xls','Sheet1');
+[xlsdata, xlstext] = xlsread('Uhlig2005_Data.xls','DATA');
 
 % Define and transform (if needed)
 dates = xlstext(2:end,1);
@@ -23,6 +23,7 @@ vnames = xlstext(1,2:end);
 X(:,1:3) = log(xlsdata(:,1:3))*100;
 X(:,4)   = xlsdata(:,4);
 X(:,5:6) = log(xlsdata(:,5:6))*100;
+X = xlsdata(:,2:end);
 VARnvar = size(X,2);
 
 %% VAR ESTIMATION
@@ -30,7 +31,7 @@ VARnvar = size(X,2);
 % Define number of variables and of observations
 [nobs, nvar] = size(X);
 % Set deterministics for the VAR
-det = 0;
+det = 1;
 % Set number of nlags
 nlags = 12;
 % Estimate VAR
@@ -45,34 +46,37 @@ VARopt.ndraws = 500;
 VARopt.snames = {'MonPol Shock'};
 VARopt.vnames = vnames;
 
-
 % Define sign restrictions : positive 1, negative -1, unrestricted 0
 SIGN = [ 0,0,0,0,0,0;  % Real GDP
-        -1,0,0,0,0,0;  % CPI
+        -1,0,0,0,0,0;  % Deflator
         -1,0,0,0,0,0;  % Commodity Price
-         1,0,0,0,0,0;  % Fed Fund
+         0,0,0,0,0,0;  % Total Reserves
         -1,0,0,0,0,0;  % NonBorr. Reserves
-         0,0,0,0,0,0]; % Total Reserves
+         1,0,0,0,0,0]; % Fed Fund
      
 
 % Define the number of steps the restrictions are imposed for:
 VARopt.sr_hor = 6;
-VARopt.sr_mod = 0;
 
 % Set options the credible intervals
 VARopt.pctg = 68;
-
 
 % Run sign restrictions routine
 SRout = SR(VAR,SIGN,VARopt);
 
 % Plot
+FigSize(20,24)
+idx = [1 3 5 2 4 6];
 for ii=1:VARnvar
-    subplot(2,3,ii)
-    plot(SRout.IRmed(:,ii,1),'-k','LineWidth',2); hold on; 
-    plot(SRout.IRinf(:,ii,1),'--k','LineWidth',1); hold on; 
-    plot(SRout.IRsup(:,ii,1),'--k','LineWidth',1); hold on; 
-    plot(zeros(VARopt.nsteps),'-k')
-    title(vnames{ii},'FontWeight','bold')
+    subplot(3,2,idx(ii))
+    PlotSwathe(SRout.IRmed(:,ii,1),[SRout.IRinf(:,ii,1) SRout.IRsup(:,ii,1)]); hold on
+    plot(zeros(VARopt.nsteps),'--k');
+    title(vnames{ii})
     axis tight
 end
+
+% Save
+SaveFigure('Figure6',1)
+
+
+
